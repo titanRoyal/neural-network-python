a2e49b5bfdc20da8eaa8686e4d9472157a838397
Gassem <titanroyal99@gmail.com>
2019-07-08 13:19:17 -0700

    more tweeks

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+# Importing the Neural network library
 from neural import *
+# Initializ the training data
 data = [
-    [1, 1],
-    [1, 0],
-    [0, 0],
     [0, 1],
+    [0, 0],
+    [1, 1],
+    [1, 0]
 ]
 label = [
-    [0],
     [1],
+    [0],
     [0],
     [1]
 ]
-model = Neural(2, [10,10,10,10,10], 1, .3)
-lomodel=model.train_batch(data, label,2000)
-print(lomodel)
+# Fiting the model
+model = Neural(2, [10], 1, .3)
+# Print the history of loss
+log = model.train_batch(data, label, 3000)
+print(log)
+# Geting the new prediction from the trained neural network
 print(model.feedforward(data, True))
--- matrix.py
+++ matrix.py
@@ -6,7 +6,7 @@ class Matrix:
     def __init__(self, x, y):
         self.row = x
         self.col = y
-        self.matrox = np.arange(x*y,dtype=np.float64).reshape(x,y)
+        self.matrox = np.arange(x * y, dtype=np.float64).reshape(x, y)
 
     def randomize(self):
         for x in range(self.row):
@@ -24,20 +24,24 @@ class Matrix:
         else:
             print("not compatible")
             np.append()
+
     def map(self, func):
         for x in range(self.row):
             for y in range(self.col):
                 self.matrox[x][y] = func(self.matrox[x][y])
+
+    def toArray(self):
+        return self.matrox.copy().flatten()
+#----------------------------all the static methods of the matrix object-----------------------------------
     @staticmethod
     def dotProduct(xx, yy):
         try:
-            tr = np.dot(xx.matrox,yy.matrox)
+            tr = np.dot(xx.matrox, yy.matrox)
             r = Matrix(tr.shape[0], tr.shape[1])
             r.matrox = tr
             return r
         except Exception as e:
             print("dimention are not compatible static")
-
 
     @staticmethod
     def fromArray(arr):
@@ -46,9 +50,6 @@ class Matrix:
         inppp = Matrix(inppp.shape[0], 1)
         inppp.matrox = tab
         return inppp
-
-    def toArray(self):
-        return self.matrox.copy().flatten()
 
     @staticmethod
     def sub(x, y):
@@ -69,7 +70,7 @@ class Matrix:
     def multElement(xx, yy):
         if xx.row == yy.row and xx.col == yy.col:
             tt = Matrix(xx.row, xx.col)
-            tt.matrox = np.multiply(xx.matrox , yy.matrox)
+            tt.matrox = np.multiply(xx.matrox, yy.matrox)
             return tt
         else:
             print("not compatible multElement")
